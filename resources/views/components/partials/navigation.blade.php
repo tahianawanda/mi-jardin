1c9f530de36edf56e447f066f43af3902254e65f
WindyD2021 <tahianawanda@gmail.com>
2024-12-11 10:45:02 -0300

    fix: ajustes generales y corrección de errores menores

---
+++ resources/views/components/partials/navigation.blade.php
@@ -0,0 +1,9 @@
+<nav>
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">My Garden</a></li>
+        <li><a href="#">Forum</a></li>
+        <li><a href="#">Nature's Map</a></li>
+        <li><a href="#">Settings</a></li>
+    </ul>
+</nav>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,3 @@
+<x-layout meta-title="Home">
+    <h1>Welcome!</h1>
+</x-layout>
